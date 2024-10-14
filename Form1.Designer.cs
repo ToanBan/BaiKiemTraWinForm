9179ea95c80e027342498f49e551365b23d5bc3b
letoanban <letoanban.word@gamil.com>
2024-10-14 09:51:24 +0700

    push bai kiem tra winform

---
+++ Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace BaiKiemTra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblMa = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblNgay = new System.Windows.Forms.Label();
+            this.lblLoai = new System.Windows.Forms.Label();
+            this.txtMa = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.colMa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNgay = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colLoai = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cmbLoai = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnNotSave = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(493, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Danh Mục Sản Phẩm";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(266, 110);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(179, 22);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(482, 104);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(90, 37);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Tìm";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lblMa
+            // 
+            this.lblMa.AutoSize = true;
+            this.lblMa.Location = new System.Drawing.Point(754, 194);
+            this.lblMa.Name = "lblMa";
+            this.lblMa.Size = new System.Drawing.Size(91, 16);
+            this.lblMa.TabIndex = 4;
+            this.lblMa.Text = "Mã Sản Phẩm";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(754, 252);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(96, 16);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Tên Sản Phẩm";
+            // 
+            // lblNgay
+            // 
+            this.lblNgay.AutoSize = true;
+            this.lblNgay.Location = new System.Drawing.Point(754, 310);
+            this.lblNgay.Name = "lblNgay";
+            this.lblNgay.Size = new System.Drawing.Size(76, 16);
+            this.lblNgay.TabIndex = 6;
+            this.lblNgay.Text = "Ngày Nhập";
+            // 
+            // lblLoai
+            // 
+            this.lblLoai.AutoSize = true;
+            this.lblLoai.Location = new System.Drawing.Point(754, 374);
+            this.lblLoai.Name = "lblLoai";
+            this.lblLoai.Size = new System.Drawing.Size(98, 16);
+            this.lblLoai.TabIndex = 7;
+            this.lblLoai.Text = "Loại Sản Phẩm";
+            // 
+            // txtMa
+            // 
+            this.txtMa.Location = new System.Drawing.Point(933, 187);
+            this.txtMa.Name = "txtMa";
+            this.txtMa.Size = new System.Drawing.Size(165, 22);
+            this.txtMa.TabIndex = 8;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(933, 249);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(165, 22);
+            this.txtName.TabIndex = 9;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colMa,
+            this.colName,
+            this.colNgay,
+            this.colLoai});
+            this.dataGridView1.Location = new System.Drawing.Point(96, 187);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(639, 254);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // colMa
+            // 
+            this.colMa.HeaderText = "Mã Sản Phẩm";
+            this.colMa.MinimumWidth = 6;
+            this.colMa.Name = "colMa";
+            this.colMa.Width = 125;
+            // 
+            // colName
+            // 
+            this.colName.HeaderText = "Tên Sản Phẩm";
+            this.colName.MinimumWidth = 6;
+            this.colName.Name = "colName";
+            this.colName.Width = 125;
+            // 
+            // colNgay
+            // 
+            this.colNgay.HeaderText = "Ngày Nhập";
+            this.colNgay.MinimumWidth = 6;
+            this.colNgay.Name = "colNgay";
+            this.colNgay.Width = 125;
+            // 
+            // colLoai
+            // 
+            this.colLoai.HeaderText = "Loại Sản Phẩm";
+            this.colLoai.MinimumWidth = 6;
+            this.colLoai.Name = "colLoai";
+            this.colLoai.Width = 125;
+            // 
+            // cmbLoai
+            // 
+            this.cmbLoai.FormattingEnabled = true;
+            this.cmbLoai.Location = new System.Drawing.Point(933, 374);
+            this.cmbLoai.Name = "cmbLoai";
+            this.cmbLoai.Size = new System.Drawing.Size(165, 24);
+            this.cmbLoai.TabIndex = 11;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(933, 310);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(274, 22);
+            this.dateTimePicker1.TabIndex = 12;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(82, 483);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(92, 55);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(375, 483);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(92, 55);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "Xoá";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(226, 483);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(88, 55);
+            this.btnEdit.TabIndex = 15;
+            this.btnEdit.Text = "Sửa";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(542, 483);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(88, 55);
+            this.btnSave.TabIndex = 16;
+            this.btnSave.Text = "Lưu";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnNotSave
+            // 
+            this.btnNotSave.Location = new System.Drawing.Point(696, 483);
+            this.btnNotSave.Name = "btnNotSave";
+            this.btnNotSave.Size = new System.Drawing.Size(106, 55);
+            this.btnNotSave.TabIndex = 17;
+            this.btnNotSave.Text = "Không Lưu";
+            this.btnNotSave.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(862, 483);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(89, 55);
+            this.btnClose.TabIndex = 18;
+            this.btnClose.Text = "Thoát";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1373, 574);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnNotSave);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.cmbLoai);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtMa);
+            this.Controls.Add(this.lblLoai);
+            this.Controls.Add(this.lblNgay);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblMa);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblMa;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblNgay;
+        private System.Windows.Forms.Label lblLoai;
+        private System.Windows.Forms.TextBox txtMa;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cmbLoai;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colMa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNgay;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colLoai;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnNotSave;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Infrastructure;
+using System.Drawing;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace BaiKiemTra
+{
+    public partial class Form1 : Form
+    {
+        private Model1 dbcontext;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadDataGridView()
+        {
+            List<Sanpham> sanphams = dbcontext.Sanphams.ToList();
+            dataGridView1.Rows.Clear();
+            foreach (var sanpham in sanphams)
+            {
+                int index = dataGridView1.Rows.Add();
+                dataGridView1.Rows[index].Cells["colMa"].Value = sanpham.MaSP;
+                dataGridView1.Rows[index].Cells["colName"].Value = sanpham.TenSP;
+                dataGridView1.Rows[index].Cells["colNgay"].Value = sanpham.NgayNhap;
+                dataGridView1.Rows[index].Cells["colLoai"].Value = sanpham.LoaiSP.TenLoai;
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ResetData()
+        {
+            txtMa.Clear();
+            txtName.Clear();    
+            cmbLoai.SelectedIndex = 0;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dbcontext = new Model1();
+            List<Sanpham> sanphams = dbcontext.Sanphams.ToList();
+            List<LoaiSP> loais = dbcontext.LoaiSPs.ToList();
+            cmbLoai.DataSource = loais;
+            cmbLoai.DisplayMember = "TenLoai";
+            cmbLoai.ValueMember = "MaLoai";
+            dataGridView1.Rows.Clear();
+            foreach (var sanpham in sanphams)
+            {
+                int index = dataGridView1.Rows.Add();
+                dataGridView1.Rows[index].Cells["colMa"].Value = sanpham.MaSP;
+                dataGridView1.Rows[index].Cells["colName"].Value = sanpham.TenSP;
+                dataGridView1.Rows[index].Cells["colNgay"].Value = sanpham.NgayNhap;
+                dataGridView1.Rows[index].Cells["colLoai"].Value = sanpham.LoaiSP.TenLoai;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var result = MessageBox.Show("Bạn có muốn thoát không", "thông báo", MessageBoxButtons.YesNo);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtMa.Text) || string.IsNullOrWhiteSpace(txtName.Text))
+                {
+                    MessageBox.Show("Vui lòng nhập đầy đủ thông tin!");
+                    return;
+                }
+
+                string fullname = txtName.Text.Trim();
+                string maLoaiStr = cmbLoai.SelectedValue.ToString();  
+                DateTime ngaynhap = dateTimePicker1.Value;
+                string MaSP = txtMa.Text;
+
+                Sanpham s = new Sanpham()
+                {
+                    MaSP = MaSP,
+                    TenSP = fullname,
+                    NgayNhap = ngaynhap,
+                    MaLoai = maLoaiStr 
+                };
+
+                dbcontext.Sanphams.Add(s);
+                dbcontext.SaveChanges();
+                LoadDataGridView();
+                ResetData();
+                MessageBox.Show("Thêm Sản Phẩm Thành Công");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Thêm Sản Phẩm Không Thành Công: " + ex.Message);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                string sanphamID = (string)dataGridView1.SelectedRows[0].Cells["colMa"].Value;
+                Sanpham sanphamEdit = dbcontext.Sanphams.FirstOrDefault(s => s.MaSP == sanphamID);
+                if (sanphamEdit != null)
+                {
+                    sanphamEdit.MaSP = txtMa.Text;
+                    sanphamEdit.TenSP = txtName.Text;
+                    sanphamEdit.NgayNhap = dateTimePicker1.Value;
+                    sanphamEdit.MaLoai = cmbLoai.SelectedValue.ToString();
+                    LoadDataGridView();
+                    MessageBox.Show("Cập Nhật Sản Phẩm Thành Công");
+                    ResetData();
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                string sanphamID = (string)dataGridView1.SelectedRows[0].Cells["colMa"].Value;
+                Sanpham sanphamDelete = dbcontext.Sanphams.FirstOrDefault(s => s.MaSP == sanphamID);
+                if (sanphamDelete != null)
+                {
+                    var result = MessageBox.Show("Bạm có muốn xoá không", "Thông Báo", MessageBoxButtons.YesNo);
+                    if (result == DialogResult.Yes)
+                    {
+                        dbcontext.Sanphams.Remove(sanphamDelete);
+                        dbcontext.SaveChanges();
+                        LoadDataGridView();
+                        MessageBox.Show("Xoá Sản Phẩm Thành Công");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Sản phẩm không tồn tại.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn sản phẩm cần xóa.");
+            }
+        }
+
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string tenSearch = txtSearch.Text;
+            if (string.IsNullOrWhiteSpace(tenSearch))
+            {
+                MessageBox.Show("Vui lòng nhập tên sản phẩm cần tìm kiếm.");
+                return;
+            }
+            var sanphams = dbcontext.Sanphams
+                .Where(s => s.TenSP.Contains(tenSearch))
+                .ToList();
+            dataGridView1.Rows.Clear();
+            foreach (var sanpham in sanphams)
+            {
+                int index = dataGridView1.Rows.Add();
+                dataGridView1.Rows[index].Cells["colMa"].Value = sanpham.MaSP;
+                dataGridView1.Rows[index].Cells["colName"].Value = sanpham.TenSP;
+                dataGridView1.Rows[index].Cells["colNgay"].Value = sanpham.NgayNhap;
+                dataGridView1.Rows[index].Cells["colLoai"].Value = sanpham.MaLoai;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
